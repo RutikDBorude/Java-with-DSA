32547d842d9073367789c586f4ef4145601ec442
RutikDBorude <167895449+RutikDBorude@users.noreply.github.com>
2026-05-27 10:49:27 +0530

    Time: 1 ms (99.95%), Space: 77 MB (81.01%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum = 0;
+        int maxsum = nums[0];
+        for(int i=0;i<nums.length;i++){
+            
+            if(currsum < 0){
+                currsum = 0;
+            }
+            currsum = currsum + nums[i];
+            maxsum = Math.max(currsum,maxsum);
+            
+
+        }
+        return maxsum;
+    }
+}
