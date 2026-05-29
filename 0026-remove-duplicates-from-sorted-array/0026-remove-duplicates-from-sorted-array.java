47e28e0167865b186e38cb35035771140ae619e7
RutikDBorude <167895449+RutikDBorude@users.noreply.github.com>
2026-05-29 14:05:41 +0530

    Time: 0 ms (100%), Space: 46.9 MB (22.71%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0) return 0;
-        int i=0;
-        for(int j=1;j<nums.length;j++){
-            if(nums[i] != nums[j]){
-                i++;
-                nums[i] = nums[j];
-                
+        int write = 0;
+        int read = 1;
+        while(read < nums.length){
+            if(nums[read] != nums[write]){
+                nums[++write] = nums[read];
+                read++;
+            }else{
+                read++;
             }
         }
-        return i+1;
+        return write + 1;
     }
-    
 }
